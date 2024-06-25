58161fa4956f7ced1c39a92babb316a7eda42123
waterwa1ker <ibragimov_kep@mail.ru>
2024-06-25 20:22:56 +0300

    fix: logic with argc and argv

--- src/containers/set/main.cc
+++ src/containers/set/main.cc
@@ -15,6 +15,9 @@
  * @return Program exit status
  */
 int main(int argc, char** argv) {
+  for (int i = 0; i < argc; ++i) {
+    std::cout << argv[i] << std::endl;
+  }
   std::cout << "Hello, world! This is an example" << std::endl;
   return 0;
 }
