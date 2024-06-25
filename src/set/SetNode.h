40cb489a243d1267f84ae0d90181e36a72252a2b
waterwa1ker <ibragimov_kep@mail.ru>
2024-06-25 17:34:43 +0300

    CI/CD testing

--- src/set/SetNode.h
+++
@@ -1,9 +0,0 @@
-namespace s21 {
-    template<typename T>
-    struct Node {
-        T value;
-        Node* left;
-        Node* right;
-        Node(T value) : value(value), left(nullptr), right(nullptr) {}
-    }Node;
-}
