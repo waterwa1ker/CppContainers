4c6f685b7e832e2b8b8d5f11280db9ffbbc390f8
waterwa1ker <ibragimov_kep@mail.ru>
2024-06-25 20:19:05 +0300

    Documentation goal added

--- src/containers/set/main.cc
+++ src/containers/set/main.cc
@@ -1,3 +1,20 @@
 #include <iostream>
 
-int main() { std::cout << "Hello, world! This is an example" << std::endl; }
+/**
+ *
+ * \file
+ *
+ * @brief Entry point
+ *
+ * Execution of the program
+ * starts here.
+ *
+ * @param argc Number of arguments
+ * @param argv List of arguments
+ *
+ * @return Program exit status
+ */
+int main(int argc, char** argv) {
+  std::cout << "Hello, world! This is an example" << std::endl;
+  return 0;
+}
