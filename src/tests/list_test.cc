3da3819026005b443553a9f3fa5c6b30cad3c3c9
Artem Oleinikov <artemoleinikov@Macbook.local>
2024-08-02 18:09:09 +0300

    feat some tests

--- src/tests/list_test.cc
+++ src/tests/list_test.cc
@@ -7,3 +7,21 @@ TEST(S21ListConstructs, Default) {
   EXPECT_EQ(l.size(), 0);
   EXPECT_EQ(*l, nullptr);
 }
+
+TEST(S21ListConstructs, sizer) {
+  s21::list<int> l(3);
+  EXPECT_EQ(l.size(), 3);
+  EXPECT_NE(*l, nullptr);
+}
+
+TEST(S21ListConstructs, initlist) {
+  s21::list<int> l{1, 2};
+  EXPECT_EQ(l.size(), 2);
+  EXPECT_EQ(*l, 1);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
